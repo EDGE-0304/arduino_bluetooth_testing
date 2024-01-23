98b27441327b1a92e08e1e6905f17c8c5fbe657f
akaChenshunCodeMaster <91108691+akaChenshunCodeMaster@users.noreply.github.com>
2024-01-22 22:35:55 -0800

    Update bluetooth_test.ino

--- bluetooth_test/bluetooth_test.ino
+++ bluetooth_test/bluetooth_test.ino
@@ -1,14 +1,24 @@
 #include <ArduinoBLE.h>
+#include <WiFiNINA.h>
 
 // Define the LED pin
-const int LED_PIN = LED_BUILTIN; // The built-in LED on your Arduino Nano 33 IoT
+const int LED_PIN = LED_BUILTIN;
 
 // BLE Service and Characteristic
-BLEService ledService("991cbd4a-2e1b-4182-bc44-ca34489f6324"); // Custom UUID
-BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLEService wifiService("991cbd4a-2e1b-4182-bc44-ca34489f6324"); // Custom UUID for WiFi service
+BLECharacteristic wifiSSIDCharacteristic("abcd1234-ab12-cd34-ef56-abcdef123456", BLERead | BLEWrite, 32); // for SSID
+BLECharacteristic wifiPasswordCharacteristic("abcd5678-ab12-cd34-ef56-abcdef123457", BLERead | BLEWrite, 32); // for Password
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+
+  // Check for the presence of the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
 
   // Begin BLE initialization
   if (!BLE.begin()) {
@@ -18,13 +28,14 @@ void setup() {
 
   // Set BLE local name and service
   BLE.setLocalName("ArduinoNano33IoT");
-  BLE.setAdvertisedService(ledService);
+  BLE.setAdvertisedService(wifiService);
 
-  // Add characteristic to service
-  ledService.addCharacteristic(switchCharacteristic);
+  // Add characteristics to service
+  wifiService.addCharacteristic(wifiSSIDCharacteristic);
+  wifiService.addCharacteristic(wifiPasswordCharacteristic);
 
   // Add service
-  BLE.addService(ledService);
+  BLE.addService(wifiService);
 
   // Start advertising
   BLE.advertise();
@@ -40,8 +51,59 @@ void loop() {
     Serial.print("Connected to central: ");
     Serial.println(central.address());
 
-    // Blink LED while connected
+    // Once connected, stay connected until the central disconnects
     while (central.connected()) {
+      if (wifiSSIDCharacteristic.written() && wifiPasswordCharacteristic.written()) {
+        // Receive WiFi credentials
+        int ssidLength = wifiSSIDCharacteristic.valueLength();
+        int passwordLength = wifiPasswordCharacteristic.valueLength();
+
+        char ssid[ssidLength + 1];
+        char password[passwordLength + 1];
+
+        memcpy(ssid, wifiSSIDCharacteristic.value(), ssidLength);
+        ssid[ssidLength] = '\0';
+
+        memcpy(password, wifiPasswordCharacteristic.value(), passwordLength);
+        password[passwordLength] = '\0';
+
+        // Debugging: Print received credentials
+        Serial.print("Received WiFi credentials: SSID: ");
+        Serial.print(ssid);
+        Serial.print(", Password: ");
+        Serial.println(password);
+
+        // Restart WiFi module
+        WiFi.end();
+        delay(1000); // Short delay before reconnecting
+
+        // Attempt to connect to WiFi network
+        Serial.println("Connecting to WiFi...");
+        WiFi.begin(ssid, password);
+
+        int status = WiFi.status();
+        while (status != WL_CONNECTED) {
+          delay(1000);
+          Serial.print(".");
+          status = WiFi.status();
+        }
+
+        if (status == WL_CONNECTED) {
+          Serial.println("Connected to WiFi!");
+        } else {
+          Serial.println("Failed to connect to WiFi. Check credentials and signal.");
+        }
+        
+        // Reset the written flag, so we don't reprocess the same credentials
+        wifiSSIDCharacteristic.writeValue((uint8_t*)ssid, ssidLength);
+        wifiPasswordCharacteristic.writeValue((uint8_t*)password, passwordLength);
+
+        // Do something with the WiFi connection here
+        // ...
+
+      }
+
+      // Blink LED while connected
       digitalWrite(LED_PIN, HIGH);
       delay(500);
       digitalWrite(LED_PIN, LOW);
